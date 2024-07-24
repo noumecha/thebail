f733ca43e28a845197790e75878b8238eadf0cd0
noumecha <noumechaivan@gmail.com>
2024-07-24 11:46:12 +0200

    maj : adding popup

--- apps/baux/urls.py
+++ apps/baux/urls.py
@@ -11,16 +11,20 @@ urlpatterns = [
     path("baux", views.index, name='Home'),
     #path("bailleur/add/", views.bailleur, name='bailleur'),
     path("bailleur/add/", BailleurView.as_view(template_name="baux/bailleur.html"), name='bailleur'),
+    path("bailleur/list/", BailleurView.as_view(template_name="baux/bailleur_list.html"), name='bailleur_list'),
     #path("immeuble/add/", views.immeuble, name='immeuble'),
     path("immeuble/add/", ImmeubleView.as_view(template_name="baux/immeuble.html"), name='immeuble'),
     #path("Menuimmeuble/add/", views.Menuimmeuble, name='Menuimmeuble'),
     path("Menuimmeuble/add/", views.Menuimmeuble, name='Menuimmeuble'),
     #path("contrat/add/", views.contrat, name='contrat'),
     path("contrat/add/", ContratView.as_view(template_name="baux/contrat.html"), name='contrat'),
+    path("contrat/list/", ContratView.as_view(template_name="baux/contrat_list.html"), name='contrat_list'),
     #path("locataire/add/", views.locataire, name='locataire'),
     path("locataire/add/", LocataireView.as_view(template_name="baux/locataire.html"), name='locataire'),
+    path("locataire/list/", LocataireView.as_view(template_name="baux/locataire_list.html"), name='locataire_list'),
     #path("localisation/add/", views.localisation, name='localisation'),
     path("localisation/add/", LocalisationView.as_view(template_name="baux/localisation.html"), name='localisation'),
+    path("localisation/list/", LocalisationView.as_view(template_name="baux/localisation_list.html"), name='localisation_list'),
 
 
 ]
